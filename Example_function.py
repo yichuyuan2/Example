16148d844c9f2b36cd925934347587676ae702bc
yichuyuan2 <yichuyuan2@gmail.com>
2019-07-21 15:59:59 +0800

    Example_function

--- Example_function.py
+++ Example_function.py
@@ -7,3 +7,11 @@ def exchangeRate(dollar):
 print(exchangeRate(10))
 print(exchangeRate.__doc__)
 print(help(exchangeRate))
+
+def test(*params, extra = "8"):
+    print("收集参数是：", params)
+    print("位置参数是：", extra)
+    print("第二个参数是：", params[1])
+
+a = [1,2,3,4,5,6,7,8]
+print(test(*a))
